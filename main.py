777b674e4ccf8dcc779ff59eedae144363c17e4e
Asus <nenascheff.an@yandex.ru>
2021-11-23 21:37:56 +0300

    k

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('kjjinjhj')
+    print_hi('000000000000')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
