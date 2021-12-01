d5e6c333698dead1dc501ef8f8443f94e437d81e
Asus <nenascheff.an@yandex.ru>
2021-12-01 19:08:40 +0300

    Tab count

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-class YotClub:
-    def __init__(self, cool):
-        self.cool_inv = 100 - cool
-
-    def get_cool(self):
-        return self.cool_inv
-
-    def change_cool(self, cool):
-        self.cool_inv = 100 - cool
+import pygame
 
 
-res = YotClub(900)
-print(res.get_cool())
-res.change_cool(2345)
-print(res.get_cool())
+class React:
+    def __init__(self, obj):
+        self.obj = obj
+        self.x_star = 0
+        self.y_start = 0
+        pygame.draw.rect(self.obj, (34, 245, 76),
+                         (self.x_star, self.y_start, 100, 100))
+
+    def update(self, rel):
+        self.x_star += rel[0]
+        self.y_start += rel[1]
+        pygame.draw.rect(self.obj, (34, 245, 76),
+                         (self.x_star, self.y_start, 100, 100))
+
+    def get_pos(self):
+        return self.x_star, self.y_start, self.x_star + 100, self.y_start + 100
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 800
+    screen = pygame.display.set_mode(size)
+    react = React(screen)
+    running = True
+    clicked = False
+    while running:
+        # внутри игрового цикла ещё один цикл
+        # приема и обработки сообщений
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                clicked = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                react_pos = react.get_pos()
+                click_pos = event.pos
+                if react_pos[0] <= click_pos[0] <= react_pos[2]:
+                    if react_pos[1] <= click_pos[1] <= react_pos[3]:
+                        clicked = True
+            if event.type == pygame.WINDOWEXPOSED:
+                print(4)
+
+
+        pygame.display.flip()
+    pygame.quit()
